cc10346ef2e15987d62eabbb809281eb8bc48a71
Lenny Le'Mashon <leonardlemashon@gmail.com>
2025-01-08 18:08:47 +0300

    Views changes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+
+from products.models import Products
 
 # Create your views here.
+@csrf_exempt
+def list_products(request):
+    if request.method == 'GET':
+        products = list(products.objects.values())
+        return JsonResponse(products, safe=False)
+    
+
+@csrf_exempt
+def add_product(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        product = Products.objects.create(
+            name = data['name'],
+            description = data['description'],
+            price = data['price'],
+            stock = data['stock']
+        )
+        return JsonResponse(
+            {
+                'id': product.id,
+                'message': 'Product added successfully'
+            }
+        )
+    
+
+@csrf_exempt
+def update_product(request, product_id):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        product = get_object_or_404(Products, id = product_id)
+        product.name = data.get('name', product.name)
+        product.description = data.get('description', product.description)
+        product.price = data.get('price', product.price)
+        product.stock = data.get('stock', product.stock)
+        product.save()
+        return JsonResponse(
+            {
+                'message': 'Product updated successfully'
+            }
+        )
+
+@csrf_exempt
+def delete_product(request, product_id):
+    if request.method == 'DELETE':
+        product = get_object_or_404(Products, id = product_id)
+        product.delete()
+        return JsonResponse(
+            {
+                'message': 'Product deleted successfully'
+            }
+        )
